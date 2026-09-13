70f6db785710efaed1a3ba030ce58303eed5f7b7
ppreethi13 <167081571+ppreethi13@users.noreply.github.com>
2026-09-13 23:02:33 +0530

    Add files via upload

---
+++ DWSlogin.java
@@ -0,0 +1,42 @@
+package dataelement;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DWSlogin {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("./src/test/resources/DDT/commondata.properties");
+		Properties p = new Properties();
+		p.load(fis);
+		String browser = p.getProperty("browser");
+		String url=p.getProperty("url");
+		String username =p.getProperty("username");
+		String password = p.getProperty("password");
+		
+		WebDriver driver = null;
+		if(browser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}
+		if(browser.equals("edge")) {
+			driver = new EdgeDriver();
+		}
+		if(browser.equals("firefox")) {
+			driver = new FirefoxDriver();	
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get(url);
+		driver.findElement(By.xpath("//a[text()='Log in']")).click();
+		driver.findElement(By.id("Email")).sendKeys("ppreethi1311@gmail.com");
+		driver.findElement(By.id("Password")).sendKeys("preethi@123");
+		driver.findElement(By.cssSelector("[value='Log in']")).click();
+	}
+
+}
